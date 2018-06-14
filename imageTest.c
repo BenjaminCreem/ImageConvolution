3ef1525ff31f44ca4f857c652f95c36d7d3e05d7
creemb <creemb@wit.edu>
2018-06-14 17:28:28 -0400

    Cleaned up a lot of stuff. End result of current thinking should actually work and be much simpler. Still need to compute kernelSize based on k and determine how to calculate product

--- imageTest.c
+++ imageTest.c
@@ -7,101 +7,75 @@ extern void matToImage(char* name, int* mat, int* dims);
 
 int main(int argc, char** argv){
 
-    int *mat;
-    int *tempmat;
-    int *kernel;
-    char *name="image.jpg";
-    int *dims;
-    int kernelSize = 9;
+    int *mat; //matrix to store image in 
+    int *tempmat; //matrix to hold new values for the image in
+    int *kernel; //kernel matrix
+    char *name="image.jpg"; //image to conv
+    int *dims; //dimensions of image
+    int k = 1;
+    int kernelSize = 9; //size of kernel matrix
     dims=(int*) malloc(sizeof(*dims)*2);
     kernel=(int*) malloc(sizeof(*kernel)*kernelSize); //3x3
-    for(int i = 0; i < 9; i++)
+    for(int i = 0; i < kernelSize; i++)
     {
         kernel[i] = 0;
+        if(i == kernelSize/2)
+        {
+            kernel[i] = 1;
+        }
     }
-    kernel[4] = 1;
 
+    //printing the kernel matrix
     for(int i = 0; i < kernelSize; i++)
     {
         printf("%d ", kernel[i]);
+        if((i+1)%(int)sqrt((double)kernelSize) == 0)
+        {
+            printf("\n");
+        }
     }
 
+    //convert image to matrix
     mat=imageToMat(name,dims);
+    //matrix with new image
     tempmat = (int*) malloc(sizeof(*tempmat)*dims[0]*dims[1]);
-    for(int i = 0; i < dims[0]*dims[1]; i++)
-    {
-        tempmat[i] = mat[i];
-    }
 
     printf("Dims: %d %d\n",dims[0],dims[1]);
 
     //do stuff with the matrix
-    
-
     //for each pixel
-    for(int i = 0; i < dims[0]*dims[1]; i++)
+    for(int i = 0; i < dims[0]; i++)
     {
-        //Get neighborhood matrix
-        int *local = (int*) malloc(sizeof(*local)*kernelSize);
-
-        //if(i <= dims[0] || i%dims[1] == 0 || i%(dims[1]-1) == 0 || i > dims[0]*dims[1] -dims[0])
-        //{
-         //   tempmat[i] = 0;
-        //}
-        //else
-        //{
-        //for(int j = 0; j <= kernelSize; j++)
-        //{
-            //if(j >= 0 && j <= 2)
-            //{
-                //for(int k = -1; k <= 1; k++)
-                //{
-                //    local[j] = mat[i-dims[0]+k];
-              //  }
-            //}
-            //if(j >= 3 && j <= 5)
-            //{
-                //for(int k = -1; k <= 1; k++)
-                //{
-                //    local[j] = mat[i+k];
-              //  }
-            //}
-            //else
-            //{
-                //for(int k = -1; k <= 1; k++)
-                //{
-                //    local[j] = mat[i+dims[0]+k];
-              //  }
-           // }
-        //}
-
-
-
-        //Multiply by kernel
-        int *result = (int*) malloc(sizeof(*result)*kernelSize);
-        for(int j = 0; j < kernelSize; j++)
+        for(int j = 0; j < dims[1]; j++)
         {
-            result[j] = 0;
-            for(int k = 0; k < kernelSize; k++)
+            //Get neighborhood matrix
+            //int *local = (int*) malloc(sizeof(*local)*kernelSize);
+            //int adjustVal = (int)((0.5*(double)kernelSize)-0.5);
+            int sum = 0;
+            int product = 0;
+            int newVal;
+            //Make sure we wont go past the edge of the image
+            if(i-k<0 || j-k<0 || i+k>dims[1] || j+k>dims[1])
             {
-                result[j] += kernel[j]*local[k];
+                newVal = 0;
             }
-        }
-        
-        //Sum all values in result of multiplication
-        int sum = 0;
-        for(int j = 0; j < kernelSize; j++)
-        {
-            sum += result[j];
-        }
-        
-        //Divide by size of kernel
-        sum = sum / kernelSize;
-
-        //Set as new pixel 
-        tempmat[i] = sum;
+            else //Otherwise, compute new matrix value
+            {
+                for(int x=-k; x<= k; x++)
+                {
+                    for(int y=-k; y<=k; y++)
+                    {
+                        
+                    }
+                    sum += product;
+                }
+            }
+            
         }
     }
+
+
+
 
     matToImage("newImage.jpg",tempmat,dims);
 
